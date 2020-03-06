e925d295f3793d687d225f4efbd99e2db29ab992
Jafeht <jasordev@gmail.com>
2020-03-06 07:03:39 -0400

    empezando con el patron modulo y escuchando los event de los inputs y boton

--- app.js
+++ app.js
@@ -1,31 +1,79 @@
+
+
 var budgetController = (function() {
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-  return {
-
-    //return a method
-    publicTest: function(b) {
-     return add(b);
-    }
-  }
+ 
 })(); 
 
 
+
+
 var UIController = (function() {
-  
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // sera un incremento o decremento
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  };
 })();
 
 
-var controller = (function(budgetCtrl, UICtrl) {
-  var z = budgetCtrl.publicTest(5);
 
-  return {
-    anotherPublic: function() {
-      console.log(z)
-    }
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+  var setupEventListerners = function() {
+    var DOM = UICtrl.getDOMstrings();
+
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if(event.keyCode === 13 || event.which === 13) {
+        console.log('SE PRESIONA LA TECLA ENTER');
+        ctrlAddItem();
+  
+      }
+  
+    });
   }
 
+ 
+
+  var ctrlAddItem = function() {
+    // 1 obtener datos del input data
+    var input = UIController.getInput();
+    console.log(input)
+    // agregar dato al budget controller
+
+    // 3 agregar los datos a la ui
+
+    // 4 calcular el presupuesto
+
+    // 5 mostrar el presupuero en la ui
+
+    console.log('Si funciona')
+  };
+
+  return {
+    init: function() {
+      console.log('la aplicacion inicio');
+      setupEventListerners();
+    }
+  };
 })(budgetController, UIController);
+
+
+controller.init();
